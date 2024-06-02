19108b909a464e5d79dc87fc1a004776c2c4b445
Nithindas004 <nithindas232001@gmail.com>
2024-06-02 16:29:15 +0530

    db changed

--- src/api/v1/db/db.ts
+++ src/api/v1/db/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const dbConnection = async () => {
     await mongoose
-    .connect("mongodb+srv://nithindas1234:1234nith@cluster0.lvn9hia.mongodb.net/phcDb?retryWrites=true&w=majority")
+    .connect("mongodb+srv://nithindas1234:1234nith@cluster0.lvn9hia.mongodb.net/phcDb?retryWrites=true&w=majority",{useNewUrlParser: true})
     .then(() => {
         console.log("Connected to database");
     })
